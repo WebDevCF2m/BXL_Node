96f4c79655c281433f9650bcb4513cd796d14173
WebDevCF2m <gitweb@cf2m.be>
2024-09-16 13:58:41 +0200

    Modification du formulaire de contact

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ app.get('/link',function(requete,reponse){
     reponse.render('liens.ejs', {nomPage: 'Liens'});
 });
 
+// Ajouter un middleware body-parser qui analyse les données envoyées par la requête POST (lors de l'envoi du formulaire)
+const urlEncodedParser = express.urlencoded({ extended:false });
+
+// Ajouter une route pour envoyer les données du formulaire vers la page de traitement
+app.post('/traitement', urlEncodedParser, function(req,res,next){
+    // récupération des données du formulaire
+    let lenom = req.body.nom;
+    let leprenom = req.body.prenom;
+    let laville = req.body.ville;
+    let lemail = req.body.email;
+    let lemessage = req.body.message;
+
+    res.render('merci.ejs',{nomPage:'Merci pour le message',nom:lenom,prenom:leprenom,ville:laville,email:lemail,msg:lemessage});
+});
+
+
 // définir une "page" gérant l'erreur 404
 app.use(function(requete,reponse,next){
     reponse.status(404).render('page-404.ejs', {nomPage: 'Erreur 404'});
